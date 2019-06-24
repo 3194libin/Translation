9e8144bcdaf53e65e703c8e8e4f7fe1812369fab
3194libin <15764747737@qq.com>
2019-06-25 00:26:32 +0800

    翻译

--- Translation.py
+++ Translation.py
@@ -1,8 +1,11 @@
 import urllib.request
 import urllib.parse
+import json
+
+content = input('请输入需要翻译的内容：')
 url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
 data = {}
-data['i']='I Love Python!'
+data['i']=content
 data['from']= 'AUTO'
 data['to']='AUTO'
 data['smartresult']='dict'
@@ -19,4 +22,6 @@ data['action']='FY_BY_CLICKBUTTION'
 data = urllib.parse.urlencode(data).encode('utf-8')
 response = urllib.request.urlopen(url,data)
 html = response.read().decode('utf-8')
-print(html)
+
+target = json.loads(html)
+print("翻译结果： %s"%(target['translateResult'][0][0]['tgt']))
